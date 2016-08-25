4168cb3f18141cba8bd1e7948bb88815e366a34e
Mateusz Klimiuk <mateusz.klimiuk@capgemini.com>
2016-08-25 14:10:38 +0200

    Change annotation

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,9 +1,13 @@
 package com.mycompany.app;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
 /**
  * Hello world!
  *
  */
+@Service
 public class App 
 {
     public String thisIsTheText()
--- src/main/java/com/mycompany/app/AppConfig.java
+++ src/main/java/com/mycompany/app/AppConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 	
-	@Bean
+	
 	public App App(){
 		return new App();
 	}
